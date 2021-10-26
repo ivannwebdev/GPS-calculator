2ca726314770d40d807617fb2216ecc174d00646
ivan <curkanivan493@gmail.com>
2021-10-26 10:39:54 +0300

    finally

--- server/routes/getDistance.js
+++ server/routes/getDistance.js
@@ -1,7 +1,7 @@
-import { Router } from 'express';
+import express from 'express';
 import  getDistanceInKm  from './../distance/index.js';
 
-const router = Router().get('/distance', (req, res) => {
+const router = express.Router().get('/distance', (req, res) => {
     const {lon1, lon2, lat1, lat2} = req.query;
     const distance = getDistanceInKm(lon1, lat1, lon2, lat2)
     res.json({distance: `${distance} km`})
